373f80aef93673d6ad7e0789c767b8e64994957c
Seyed Samad <ssamadgh@gmail.com>
2021-03-20 09:01:42 +0330

    some imporve in markdowns

--- {module}View.swift
+++ {module}View.swift
@@ -9,18 +9,35 @@ import Foundation
 import UIKit
 
 final class {module}View: UIViewController, ViewInterface {
-
-    var presenter: {module}PresenterViewInterface!
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        self.presenter.viewDidLoad()
-    }
-
+	
+	var presenter: {module}PresenterViewInterface!
+	
+	// MARK: - Properties
+	
+	
+	
+	// MARK: - Lifecycle
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		self.applyTheme()
+		self.presenter.viewDidLoad()
+	}
+	
+	
+	// MARK: - Theme
+	
+	func applyTheme() {
+		
+	}
+	
+	
+	// MARK: - Actions
+	
+	
 }
 
 extension {module}View: {module}ViewInterface {
-
+	
 }
