9aa13a84d00f3d5bb54a96867b2b4ab369353287
Seyed Samad <ssamadgh@gmail.com>
2021-08-19 17:10:00 +0430

    some fixes for localization

--- Localized/{Name}Strings.swift
+++ Localized/{Name}Strings.swift
@@ -12,11 +12,11 @@ public struct {Name}Strings {
 }
 
 public extension {Name}Strings {
-	static func localized(_ key: String, _ comment: String?) -> String {
+	static func localizedString(_ key: String, _ comment: String?) -> String {
 		NSLocalizedString(key, tableName: "{Name}Localized", comment: comment ?? "")
 	}
 
-    static func localized(_ key: LocalizedStringKey, _ comment: StaticString?) -> Text {
+    static func localizedText(_ key: LocalizedStringKey, _ comment: StaticString?) -> Text {
         Text(key, tableName: "{Name}Localized", comment: comment)
     }
 }
