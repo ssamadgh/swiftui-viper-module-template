8448c421c28aa386a1c2e935fd1acf650cef7761
Seyed Samad <ssamadgh@gmail.com>
2021-11-05 08:58:09 +0330

    some improves on viper parts module

---
+++ Router/{Name}Router.swift
@@ -0,0 +1,18 @@
+//
+//  {Name}Router.swift
+//  {project}
+//
+//  Created by {author} on {date}.
+//
+
+import SwiftUI
+
+final class {Name}Router: RouterInterface {
+
+    weak var presenter: {Name}PresenterRouterInterface!
+
+}
+
+extension {Name}Router: {Name}RouterInterface {
+
+}
---
+++ Router/{Name}RouterInterface.swift
@@ -0,0 +1,12 @@
+//
+//  {Name}RouterInterface.swift
+//  {project}
+//
+//  Created by {author} on {date}.
+//
+
+import SwiftUI
+
+protocol {Name}RouterInterface: RouterPresenterInterface {
+
+}
